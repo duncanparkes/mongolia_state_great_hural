1f23379877a290ffc6fa551371d6e41bbb1a0bcb
Duncan Parkes <duncan@mysociety.org>
2015-05-06 14:04:44 +0100

    Remove commented out code.

--- scraper.py
+++ scraper.py
@@ -61,24 +61,3 @@ for image, member_dict in members_by_image_url.items():
     member_dict['image'] = image
     scraperwiki.sqlite.save(unique_keys=('image',), data=member_dict)
 
-        # data = {
-        #     'image': image,
-        #     'name': name,
-        #     }
-
-
-        # ### Scraperwiki saving bits below.
-
-        # scraperwiki.sqlite.save(unique_keys=('image',), data=data)
-
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
-
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".

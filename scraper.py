4d45e7c9c9a9bd47f5c0c93ee24f6a4c9264b9b4
Duncan Parkes <duncan@mysociety.org>
2015-05-06 11:59:18 +0100

    Use image as key, get names in Mongolian and English.

--- scraper.py
+++ scraper.py
@@ -4,26 +4,72 @@
 import scraperwiki
 import requests
 from urlparse import urljoin
+from urllib import urlencode
 import lxml.html
-source_url = "http://www.parliament.mn/en/who?type=3"
-response = requests.get(source_url)
 
-root = lxml.html.fromstring(response.content)
+en_source_url = "http://www.parliament.mn/en/who"
+mn_source_url = "http://www.parliament.mn/who"
 
-cv_divs = root.cssselect("div.cvListItem")
+types = (
+    # ('chairman', 1),
+    # ('vice_chairman', 2),
+    ('member', 3),
+    )
 
-for cv_div in cv_divs:
-    name = cv_div.find('div').text_content().strip()
-    image = urljoin(source_url, cv_div.getprevious().get('src'))
+members_by_image_url = {}
 
-    data = {
-        'name': name,
-        'image': image,
-        }
+for type_name, type_id in types:
+    query = {'type': type_id}
+    query_string = '?' + urlencode(query)
 
-    ### Scraperwiki saving bits below.
+    # First get all the members in Mongolian
 
-    scraperwiki.sqlite.save(unique_keys=('name',), data=data)
+    mn_response = requests.get(mn_source_url + query_string)
+    mn_root = lxml.html.fromstring(mn_response.content)
+
+    cv_divs = mn_root.cssselect("div.cvListItem")
+
+    for cv_div in cv_divs:
+        image = urljoin(mn_source_url, cv_div.getprevious().get('src'))
+        member_dict = members_by_image_url.setdefault(image, {})
+        member_dict['name_mn'] = cv_div.find('div').text_content().strip()
+
+    # Now repeat in English, matching on the images.
+
+    en_response = requests.get(en_source_url + query_string)
+    en_root = lxml.html.fromstring(en_response.content)
+
+    cv_divs = en_root.cssselect("div.cvListItem")
+
+    for cv_div in cv_divs:
+        image_rel = cv_div.getprevious().get('src')
+
+        # Strip off the language code to make this match the Mongolian
+        if image_rel.startswith('/en'):
+            image_rel = image_rel[3:]
+
+        image = urljoin(mn_source_url, image_rel)
+
+        # Putting image inside the per member dict as well as using it as a
+        # key to find it for convenience later
+        member_dict = members_by_image_url.setdefault(image, {})
+
+        member_dict['name_en'] = cv_div.find('div').text_content().strip()
+
+
+for image, member_dict in members_by_image_url.items():
+    member_dict['image'] = image
+    scraperwiki.sqlite.save(unique_keys=('image',), data=member_dict)
+
+        # data = {
+        #     'image': image,
+        #     'name': name,
+        #     }
+
+
+        # ### Scraperwiki saving bits below.
+
+        # scraperwiki.sqlite.save(unique_keys=('image',), data=data)
 
 # # Write out to the sqlite database using scraperwiki library
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})

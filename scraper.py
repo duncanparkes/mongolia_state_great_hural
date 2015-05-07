26bf01c1aaeb4468942c08271e029449a685ac88
Duncan Parkes <duncan@mysociety.org>
2015-05-07 08:57:01 +0100

    Only one type needed, so simplify.

--- scraper.py
+++ scraper.py
@@ -10,66 +10,59 @@ import lxml.html
 en_source_url = "http://www.parliament.mn/en/who"
 mn_source_url = "http://www.parliament.mn/who"
 
-types = (
-    # ('chairman', 1),
-    # ('vice_chairman', 2),
-    ('member', 3),
-    )
-
 members_by_image_url = {}
 
-for type_name, type_id in types:
-    query = {'type': type_id}
-    query_string = '?' + urlencode(query)
+query = {'type': 3} # 3 is members
+query_string = '?' + urlencode(query)
 
-    # First get all the members in Mongolian
+# First get all the members in Mongolian
 
-    mn_response = requests.get(mn_source_url + query_string)
-    mn_root = lxml.html.fromstring(mn_response.text)
+mn_response = requests.get(mn_source_url + query_string)
+mn_root = lxml.html.fromstring(mn_response.text)
 
-    cv_lists = mn_root.cssselect("div#cvList")
+cv_lists = mn_root.cssselect("div#cvList")
 
-    for cv_list in cv_lists:
-        for a in cv_list:
-            homepage_src = urljoin(mn_source_url, a.get('href'))
+for cv_list in cv_lists:
+    for a in cv_list:
+        homepage_src = urljoin(mn_source_url, a.get('href'))
 
-            cv_div = a.cssselect('div.cvListItem')[0]
+        cv_div = a.cssselect('div.cvListItem')[0]
 
-            image = urljoin(mn_source_url, a.cssselect('img')[0].get('src'))
-            member_dict = members_by_image_url.setdefault(image, {})
-            member_dict['name_mn'] = cv_div.find('div').text_content().strip()
+        image = urljoin(mn_source_url, a.cssselect('img')[0].get('src'))
+        member_dict = members_by_image_url.setdefault(image, {})
+        member_dict['name_mn'] = cv_div.find('div').text_content().strip()
 
-            homepage_src = urljoin(mn_source_url, a.get('href'))
-            home_resp = requests.get(homepage_src)
-            home_root = lxml.html.fromstring(home_resp.content)
+        homepage_src = urljoin(mn_source_url, a.get('href'))
+        home_resp = requests.get(homepage_src)
+        home_root = lxml.html.fromstring(home_resp.content)
 
-            email = home_root.cssselect('a[href^="mailto"]')[0].get('href')
-            if email.startswith('mailto:'):
-                member_dict['email'] = email[7:]
+        email = home_root.cssselect('a[href^="mailto"]')[0].get('href')
+        if email.startswith('mailto:'):
+            member_dict['email'] = email[7:]
 
-    # Now repeat in English, matching on the images.
+# Now repeat in English, matching on the images.
 
-    en_response = requests.get(en_source_url + query_string)
-    en_root = lxml.html.fromstring(en_response.text)
+en_response = requests.get(en_source_url + query_string)
+en_root = lxml.html.fromstring(en_response.text)
 
-    cv_lists = en_root.cssselect("div#cvList")
+cv_lists = en_root.cssselect("div#cvList")
 
-    for cv_list in cv_lists:
-        for a in cv_list:
-            image_rel = a.cssselect('img')[0].get('src')
+for cv_list in cv_lists:
+    for a in cv_list:
+        image_rel = a.cssselect('img')[0].get('src')
 
-            # Strip off the language code to make this match the Mongolian
-            if image_rel.startswith('/en'):
-                image_rel = image_rel[3:]
+        # Strip off the language code to make this match the Mongolian
+        if image_rel.startswith('/en'):
+            image_rel = image_rel[3:]
 
-            image = urljoin(mn_source_url, image_rel)
+        image = urljoin(mn_source_url, image_rel)
 
-            # Putting image inside the per member dict as well as using it as a
-            # key to find it for convenience later
-            member_dict = members_by_image_url.setdefault(image, {})
+        # Putting image inside the per member dict as well as using it as a
+        # key to find it for convenience later
+        member_dict = members_by_image_url.setdefault(image, {})
 
-            cv_div = a.cssselect('div.cvListItem')[0]
-            member_dict['name_en'] = cv_div.find('div').text_content().strip()
+        cv_div = a.cssselect('div.cvListItem')[0]
+        member_dict['name_en'] = cv_div.find('div').text_content().strip()
 
 
 for image, member_dict in members_by_image_url.items():
